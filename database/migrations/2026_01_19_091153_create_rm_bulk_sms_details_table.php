a251a884c3d176b29c01c60b8633e890a6479c2c
Abu Shahadat Md. Sayem <sayem1413@gmail.com>
2026-01-19 15:22:07 +0600

    migration and model init

---
+++ database/migrations/2026_01_19_091153_create_rm_bulk_sms_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rm_bulk_sms_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('to');
+            $table->string('from')->nullable();
+            $table->text('message');
+            $table->string('gateway');
+            $table->string('provider_message_id')->nullable();
+            $table->enum('status', ['queued', 'sent', 'delivered', 'failed'])->default('queued');
+            $table->json('provider_response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rm_bulk_sms_details');
+    }
+};
