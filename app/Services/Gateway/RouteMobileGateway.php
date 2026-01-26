38a8769cd75b0b9d3cd13e84d53bd679d4373a04
Abu Shahadat Md. Sayem <sayem1413@gmail.com>
2026-01-26 10:39:56 +0600

    Store response and status from curl request

--- app/Services/Gateway/RouteMobileGateway.php
+++ app/Services/Gateway/RouteMobileGateway.php
@@ -4,39 +4,34 @@ namespace App\Services\Gateway;
 use App\Contracts\RouteMobileContract;
 use App\DTOs\SmsMessageDTO;
 use App\DTOs\RouteMobileBulkSmsDTO;
+use App\Models\RmBulkSms;
 use Illuminate\Support\Facades\Http;
 use Exception;
+
+use function Symfony\Component\Clock\now;
 
 class RouteMobileGateway implements RouteMobileContract
 {
     public function sendBulkSmsBd(RouteMobileBulkSmsDTO $dto): array
     {
-        $data = [
-            'username'    => config('services.rml_bd.username'),
-            'password'    => config('services.rml_bd.password'),
-            'dlr'         => 1,
-            'type'        => 0,
-            'source'      => config('services.rml_bd.source'),
-            'destination' => '88' . $dto->destination,
-            'message'     => $dto->message,
-        ];
+        $response = $this->curlForSendBulkSmsBD($dto);
 
-        try {
-            $response = Http::withQueryParameters($data)
-                ->acceptJson()
-                ->get(config('services.rml_bd.url'));
+        $resBody = $response['res_body'];
+        [$code, $mobile, $messageId] = explode('|', $resBody);
 
-            return [
-                'success' => $response->ok(),
-                'raw'     => $response->body(),
-            ];
+        RmBulkSms::create([
+            'to' => $mobile,
+            'message' => $dto->message,
+            'message_id' => $messageId,
+            'status' => $response['success'],
+            'status_code' => $code,
+            'response' => $response['res_body'],
+            'sent_at' => now(),
+            'delivered_at' => $response['success'] ? now() : null,
+        ]);
 
-        } catch (Exception $e) {
-            return [
-                'success' => false,
-                'raw'     => $e->getMessage(),
-            ];
-        }
+        return $response;
+
     }
 
     public function sendBulk(SmsMessageDTO $message): array
@@ -59,5 +54,35 @@ class RouteMobileGateway implements RouteMobileContract
             'status'     => $payload['status'] ?? null,
             'delivered_at' => $payload['deliveredAt'] ?? null,
         ];
+    }
+
+    private function curlForSendBulkSmsBD(RouteMobileBulkSmsDTO $dto)
+    {
+        $data = [
+            'username'    => config('services.rml_bd.username'),
+            'password'    => config('services.rml_bd.password'),
+            'dlr'         => 1,
+            'type'        => 0,
+            'source'      => config('services.rml_bd.source'),
+            'destination' => '88' . $dto->destination,
+            'message'     => $dto->message,
+        ];
+
+        try {
+            $response = Http::withQueryParameters($data)
+                ->acceptJson()
+                ->get(config('services.rml_bd.url'));
+
+            return [
+                'success' => $response->ok(),
+                'res_body'     => $response->body(),
+            ];
+
+        } catch (Exception $e) {
+            return [
+                'success' => false,
+                'res_body'     => $e->getMessage(),
+            ];
+        }    
     }
 }
